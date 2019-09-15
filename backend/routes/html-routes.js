9d3a058915825c4a2ca6df1f009c9827836e8e64
Rittika Adhikari <rittika.adhikari@gmail.com>
2019-09-15 05:19:59 -0400

    Creating endpoints for mySQL database

--- backend/routes/html-routes.js
+++ backend/routes/html-routes.js
@@ -1,9 +1,20 @@
 var Request = require('tedious').Request;
 const path = require('path')
 
-module.exports = function(app, connection) {
-    app.get('/', function(req,res) {
-        res.send("hello from simple-react-project")
-    });
-};
+module.exports = function (app, connection) {
+    app.get('/create', function (req, res) {
+        res.send(createDatabase());
+    })
 
+    app.get('/get_all', function (req, res) { 
+        res.send(readData());
+    })
+
+    app.post('/insert_data', function (req, res) {
+        res.send(insertData());
+    })
+
+    app.delete('/delete_row', function (req, res) {
+        res.send(deleteData());
+    }) 
+}
